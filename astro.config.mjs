dffb30a3feb5742848d57d7ea214b89c7711bc13
605500737 <vincent.liger@mymoneybank.com>
2026-01-28 10:53:59 +0100

    🐛Fix theme call

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,6 @@
-//// @ts-check
-//import { defineConfig } from 'astro/config';
-//import starlight from '@astrojs/starlight';
-//
-//// https://astro.build/config
-//export default defineConfig({
-//	integrations: [
-//		starlight({
-//			title: 'Wikidoc',
-//			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-//			sidebar: [
-//				{
-//					label: 'Guides',
-//					items: [
-//						// Each item here is one entry in the navigation menu.
-//						{ label: 'Example Guide', slug: 'guides/example' },
-//					],
-//				},
-//				{
-//					label: 'Reference',
-//					autogenerate: { directory: 'reference' },
-//				},
-//			],
-//		}),
-//	],
-//});
-
-// @ts-check
+import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config';
-import catppuccin from "@catppuccin/starlight";
+import starlightCatppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,7 +10,7 @@ export default defineConfig({
 			title: 'Wikidoc',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/codium9/wikidoc' }],
       plugins: [
-        catppuccin({
+        starlightCatppuccin({
           dark: { flavor: "macchiato", accent: "sky" },
           light: { flavor: "latte", accent: "sky" }
         })
