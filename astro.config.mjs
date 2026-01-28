e13990f33dd36cbc349a832f552b17f3bfb5fabc
605500737 <vincent.liger@mymoneybank.com>
2026-01-28 11:07:20 +0100

    🚧Fix docs directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,23 @@ export default defineConfig({
     starlight({
 			title: 'Wikidoc',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/codium9/wikidoc' }],
+      sidebar: [
+        { label: 'Backup', autogenerate: { directory: 'Backup' } },
+        { label: 'Code', autogenerate: { directory: 'Code' } },
+        { label: 'Database', autogenerate: { directory: 'Database' } },
+        { label: 'Docker', autogenerate: { directory: 'Docker' } },
+        { label: 'Git', autogenerate: { directory: 'Git' } },
+        { label: 'Linux', autogenerate: { directory: 'Linux' } },
+        { label: 'Microsoft', autogenerate: { directory: 'Microsoft' } },
+        { label: 'Middleware', autogenerate: { directory: 'Middleware' } },
+        { label: 'Security', autogenerate: { directory: 'Security' } },
+        { label: 'Terraform', autogenerate: { directory: 'Terraform' } },
+        { label: 'Unix', autogenerate: { directory: 'Unix' } }
+      ],
       plugins: [
         starlightCatppuccin({
-          dark: { flavor: "macchiato", accent: "sky" },
-          light: { flavor: "latte", accent: "sky" }
+          dark: { flavor: "macchiato", accent: "mauve" },
+          light: { flavor: "latte", accent: "mauve" }
         })
       ]
     })
