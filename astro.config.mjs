26d290d9209cbc67f563cf96f65c771d7d9fc891
605500737 <vincent.liger@mymoneybank.com>
2026-03-18 13:40:44 +0100

    🔧 Passage au domaine xscale

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import remarkEmoji from 'remark-emoji';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wiki.itdata.fr',
+  site: 'https://wiki.xscale.fr',
   markdown: {
     remarkPlugins: [remarkEmoji],
     shikiConfig: {
